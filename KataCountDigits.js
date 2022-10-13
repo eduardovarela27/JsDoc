eca6929a285d204814e767a131c4a630eb7181d4
Eduardo Varela <68159306+eduardovarela27@users.noreply.github.com>
2022-10-13 14:24:20 -0600

    Update KataCountDigits.js

--- KataCountDigits.js
+++ KataCountDigits.js
@@ -24,3 +24,13 @@ function nbDig(n, d) {
     }});
     return ct;
 }
+
+
+//other alternative by nabi 
+function nbDig(n, d) {
+var res=0;
+    for (var g=0;g<=n;g++){
+      var square=(g*g+"").split("");
+      square.forEach((s)=>s==d?res++:null)
+    }return res;
+}
