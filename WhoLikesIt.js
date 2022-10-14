009434fa02e456e5bdaf6655d2278191ee895633
Eduardo Varela <68159306+eduardovarela27@users.noreply.github.com>
2022-10-14 15:51:45 -0600

    Update WhoLikesIt.js

--- WhoLikesIt.js
+++ WhoLikesIt.js
@@ -15,3 +15,14 @@ function likes(names) {
   if(names.length === 3){return  names[0] + ", " + names[1] + " and " + names[2] + " like this"};
   return names.length === 2 ?  names[0] + " and " + names[1] + " like this"  : names[0] + " likes this";                 
 }
+
+//Alternative by Zadoev
+function likes(names) {
+  return {
+    0: 'no one likes this',
+    1: `${names[0]} likes this`, 
+    2: `${names[0]} and ${names[1]} like this`, 
+    3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
+    4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
+  }[Math.min(4, names.length)]
+}
