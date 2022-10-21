79c628ea434f1f2db11940c858b28bd4e995c455
Eduardo Varela <68159306+eduardovarela27@users.noreply.github.com>
2022-10-21 10:17:02 -0600

    Update reduce.js

--- MethodsExamples/reduce.js
+++ MethodsExamples/reduce.js
@@ -25,26 +25,26 @@ let miCarrito = [
 const miCarrito2 = [];
 let y = 1;
 miCarrito.forEach(pro => {//recorro cada objeto del arreglo
-    for (let i = y; i < miCarrito.length; i++) {//lo comparo con el siguiente
-        const element = miCarrito[i];
-        if (pro.id_producto === element.id_producto) {
-            let comp = miCarrito2.find(p => p.id_producto === element.id_producto) ?? null;
-            if (comp === null) {
-                element.cantidad += pro.cantidad;
+    for (let i = y; i < miCarrito.length; i++) {//lo comparo con el siguiente, por eso y=1
+        const element = miCarrito[i]; //recojo el valor del elemento siguiente
+        if (pro.id_producto === element.id_producto) {//Evaluo si tienen el mismo id
+            let comp = miCarrito2.find(p => p.id_producto === element.id_producto) ?? null;//busco si el elemento con ese id ya existe en el arreglo nuevo
+            if (comp === null) {//si no existe, lo agrego
+                element.cantidad += pro.cantidad; //itero la cantidad de unidades
                 miCarrito2.push({ ...element });
-                element.cantidad = 0;
-            } else if (comp) {
-                miCarrito2.forEach(m => {
+                // element.cantidad = 0;
+            } else if (comp) {//si existe, solo itero la cantidad de unidades
+                miCarrito2.forEach(m => {//recorro el nuevo arreglo
                     if (m.id_producto === comp.id_producto) {
                         m.cantidad += element.cantidad;
                         element.cantidad = 0;
                     }
                 });
             }
-        } else if (!miCarrito2.find(pro => pro.id_producto === element.id_producto)) {
+        } else if (!miCarrito2.find(pro => pro.id_producto === element.id_producto)) {//agrego elementos unicos, que no tengan elementos repetidos
             miCarrito2.push(element);
         }
     }
-    y++;
+    y++;//itero la y, la cual controla la comparacion con el elemnto siguiente
 });
 console.log(miCarrito2);
