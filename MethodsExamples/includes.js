7c8beaebb8eb18a2e761782856dac6de349699bb
Eduardo Varela <68159306+eduardovarela27@users.noreply.github.com>
2022-10-24 14:47:43 -0600

    Create includes.js

---
+++ MethodsExamples/includes.js
@@ -0,0 +1,11 @@
+[1, 2, 3].includes(3, 3);  // false  (searchElement, [fromindex(optional)])
+//example
+let i = 6;
+
+if(i===2 || i===3  ||  i===4 || i===5|| i===6  || i===7){
+    return "Hello world";
+ }
+
+if ([2,3,4,5,6,7].includes(i)){
+    return "Hello world, but better";
+}
